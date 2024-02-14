1b66bcf72f373592f67e8c5d7f4a92206240817b
Cleberson <pauluci.cleberson@gmail.com>
2024-02-14 15:26:40 -0300

    Started with goroutines

--- main.go
+++ main.go
@@ -15,8 +15,11 @@ func slowGreet(phrase string) {
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	// keyword go means that go will run code as goroutines (parallel)
+	go greet("Nice to meet you!")
+	go greet("How are you?")
+	go slowGreet("How ... are ... you ...?")
+	go greet("I hope you're liking the course!")
+
+	time.Sleep(4 * time.Second)
 }
